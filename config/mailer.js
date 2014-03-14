16119f3e09dffbc67c6b2077b840ff5a9b141f9c
Dimitri Jorge <jorge.dimitri@gmail.com>
2014-03-14 16:05:57 +0100

    Fix mailing

--- config/mailer.js
+++ config/mailer.js
@@ -2,18 +2,17 @@ var nodemailer = require("nodemailer");
 
 // create reusable transport method (opens pool of SMTP connections)
 var smtpTransport = nodemailer.createTransport("SMTP",{
-  host: "in.mailjet.com",
-  port: 587, // port for secure SMTP
+  service: "Mailjet",
   auth: {
-    user: "84f24ca90b7a300a8db12cf5be860eeb",
-    pass: "0f663d8a401192de25d46f6c22769e91"
+    user: process.env.MAILJET_API_KEY,
+    pass: process.env.MAILJET_SECRET_KEY
   }
 });
 
 module.exports.send_validation = function(to, subject, text, html) {
   // setup e-mail data with unicode symbols
   var mailOptions = {
-    from: "Gravacoin ✔ <admin@gravacoin.com>", // sender address
+    from: "Gravacoin Admin ✔ <jorge.dimitri@gmail.com>", // sender address
     to: to, // list of receivers
     subject: subject, // Subject line
     text: text, // plaintext body
