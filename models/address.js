248117094879324a134b1c3fdcbacb700bfb3927
Dimitri Jorge <jorge.dimitri@gmail.com>
2014-04-03 14:43:38 +0200

    Ensure email is trimed and lowercased in address model

--- models/address.js
+++ models/address.js
@@ -16,7 +16,7 @@ function generateRandomToken() {
 }
 function encryptEmail(email) {
   try {
-    return crypto.createHash('md5').update(email).digest("hex");
+    return crypto.createHash('md5').update(email.trim().toLowerCase()).digest("hex");
   } catch(e) {
     throw "Error in address model - Crypto generation failed"
   }
@@ -45,7 +45,7 @@ AddressSchema.pre('save', function(next, done) {
   // Return if it's a save instead of a create
   if (!self.isNew) return next()
 
-  self.email = self.email.toLowerCase();
+  self.email = self.email.trim().toLowerCase();
 
   self.validation_token = generateRandomToken();
   self.encrypted_email = encryptEmail(self.email);
@@ -111,26 +111,29 @@ AddressSchema.methods = {
   }
 }
 
+AddressSchema.statics.get_encrypted_email_for_email = function(email) {
+  return encryptEmail(email);
+}
 AddressSchema.statics.search_by_email_and_currency = function(email, currency_id, cb, select_pattern) {
   return this.findOne({'email': email.toLowerCase(), currency: currency_id}).select(select_pattern).populate('currency', 'name symbol url -_id').exec(cb);
 }
 AddressSchema.statics.search_by_encrypted_and_currency = function(encrypted_email, currency_id, cb, select_pattern) {
-  return this.findOne({'encrypted_email': encrypted_email.toLowerCase(), currency: currency_id}).select(select_pattern).populate('currency', 'name symbol url -_id').exec(cb);
+  return this.findOne({'encrypted_email': encrypted_email.trim().toLowerCase(), currency: currency_id}).select(select_pattern).populate('currency', 'name symbol url -_id').exec(cb);
 }
 AddressSchema.statics.search_by_encrypted_and_currency_validated = function(encrypted_email, currency_id, cb, select_pattern) {
-  return this.findOne({'encrypted_email': encrypted_email.toLowerCase(), currency: currency_id, validated: true}).select(select_pattern).populate('currency', 'name symbol url -_id').exec(cb);
+  return this.findOne({'encrypted_email': encrypted_email.trim().toLowerCase(), currency: currency_id, validated: true}).select(select_pattern).populate('currency', 'name symbol url -_id').exec(cb);
 }
 AddressSchema.statics.search_validated_by_encrypted = function(encrypted_email, cb, select_pattern) {
-  return this.find({encrypted_email: encrypted_email.toLowerCase(), validated: true}).select(select_pattern).populate('currency', 'name symbol url -_id').exec(cb);
+  return this.find({encrypted_email: encrypted_email.trim().toLowerCase(), validated: true}).select(select_pattern).populate('currency', 'name symbol url -_id').exec(cb);
 }
 AddressSchema.statics.search_by_encrypted = function(encrypted_email, cb, select_pattern) {
-  return this.find({encrypted_email: encrypted_email.toLowerCase()}).select(select_pattern).populate('currency', 'name symbol url -_id').exec(cb);
+  return this.find({encrypted_email: encrypted_email.trim().toLowerCase()}).select(select_pattern).populate('currency', 'name symbol url -_id').exec(cb);
 }
 AddressSchema.statics.search_by_encrypted_validated = function(encrypted_email, cb, select_pattern) {
-  return this.find({encrypted_email: encrypted_email.toLowerCase(), validated: true}).select(select_pattern).populate('currency', 'name symbol url -_id').exec(cb);
+  return this.find({encrypted_email: encrypted_email.trim().toLowerCase(), validated: true}).select(select_pattern).populate('currency', 'name symbol url -_id').exec(cb);
 }
 AddressSchema.statics.search_by_encrypted_not_validated = function(encrypted_email, cb, select_pattern) {
-  return this.find({encrypted_email: encrypted_email.toLowerCase(), validated: false}).select(select_pattern).populate('currency', 'name symbol url -_id').exec(cb);
+  return this.find({encrypted_email: encrypted_email.trim().toLowerCase(), validated: false}).select(select_pattern).populate('currency', 'name symbol url -_id').exec(cb);
 }
 
 mongoose.model('Address', AddressSchema);
