3c34524bb5c767b8da3de8d1013de577a7178325
Dimitri Jorge <jorge.dimitri@gmail.com>
2014-04-15 15:28:53 +0200

    Upgrade script output

--- scripts/pick_random_address.js
+++ scripts/pick_random_address.js
@@ -20,14 +20,20 @@ Currency.findOne({symbol: process.argv[2]}, function(err, currency) {
         var number = Math.floor(Math.random() * results.length);
         var winner = results[number];
 
-        // console.log("Length == " + results.length + " and random number == " + number);
+        console.log("Length == " + results.length + " and random number == " + number);
         // console.log("Picked address == " + winner);
 
         Address.find({address: winner}, function(err, addr) {
+          if (addr.length != 1) throw "Only one profile is authorized"
+
           console.log("And the winner is:")
           console.log("==================")
-          console.log(addr);
+          console.log({address: addr[0].address, hash: addr[0].encrypted_email});
           console.log("==================")
+
+
+          console.log("\n\n\n\n")
+          console.log(addr);
           mongoose.connection.close()
         });
 
