1df2cdda7765685452749832ba9f2523ea1966c1
Dimitri Jorge <jorge.dimitri@gmail.com>
2014-04-18 14:25:43 +0200

    Minify all libs and merge them together

--- Gulpfile.js
+++ Gulpfile.js
@@ -4,16 +4,39 @@ var gulp = require('gulp')
   , jshint = require('gulp-jshint')
   , mocha = require('gulp-mocha')
   , gutil = require('gulp-util')
-  , coffee = require('gulp-coffee');
+  , uglify = require('gulp-uglify')
+  , coffee = require('gulp-coffee')
+  , concat = require('gulp-concat');
+
+var libs = [
+    'lib/jquery/jquery.min.js'
+  , 'lib/angular/angular.min.js'
+  , 'lib/angular/angular-qrcode.js'
+  , 'lib/angular/angular-resource.min.js'
+  , 'lib/zeroclipboard/ZeroClipboard.min.js'
+  , 'lib/angular/ng-clip.min.js'
+  , 'lib/bootstrap/js/bootstrap.js'
+  , 'lib/qrcode-generator/qrcode.js'
+]
 
 var paths = {
-  scripts: ['./app.js', './models/*.js', 'public/javascripts/*.js', 'routes/*.js', 'config/*.js'],
+  scripts: ['./app.js', './models/*.js', 'public/javascripts/main.js', 'public/javascripts/profile.js', 'routes/*.js', 'config/*.js'],
+  libs: libs,
   tests: ['./test/*.js']
 }
+
+
 
 gulp.task('coffee', function() {
   gulp.src('./public/javascripts/*.coffee')
     .pipe(coffee({bare: true}).on('error', gutil.log))
+    .pipe(uglify())
+    .pipe(gulp.dest('./public/javascripts'))
+});
+gulp.task('merge_libs', function() {
+  gulp.src(paths.libs)
+    .pipe(uglify())
+    .pipe(concat('all_libraries.min.js'))
     .pipe(gulp.dest('./public/javascripts'))
 });
 
@@ -26,10 +49,10 @@ gulp.task('lint', function() {
 
 gulp.task('develop', function () {
   nodemon({ script: 'app.js', ext: 'html js coffee', ignore: ['test/*', 'Gulpfile.js'] })
-    .on('change', ['lint', 'coffee'])
+    .on('change', ['lint', 'coffee', 'merge_libs'])
     .on('restart', function () {
       console.log('restarted!')
     })
 })
 
-gulp.task('default', ['lint', 'coffee', 'develop']);
+gulp.task('default', ['lint', 'coffee', 'merge_libs', 'develop']);
