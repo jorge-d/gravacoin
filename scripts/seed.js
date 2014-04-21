991580bd1c8f4d904fd5e5f18dfdb0f4c48b2eaf
Dimitri Jorge <jorge.dimitri@gmail.com>
2014-04-21 16:59:09 +0200

    Add BonusCoin

--- scripts/seed.js
+++ scripts/seed.js
@@ -33,6 +33,7 @@ Currency.create(
   , {name: 'Megacoin', symbol: 'mec', url: 'http://megacoin.in'}
   , {name: 'BlackCoin', symbol: 'bc', url: 'http://blackcoin.co'}
   , {name: 'DarkCoin', symbol: 'drk', url: 'http://darkcoin.io'}
+  , {name: 'BonusCoin', symbol: 'bns', url: 'http://bonuscoin.net'}
   , handleRes
 );
 
